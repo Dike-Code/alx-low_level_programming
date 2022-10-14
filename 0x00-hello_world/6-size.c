91f038100a747f05bb251d3693822f0f87ec974c
root <dikeclintonobinna58@gmail.com>
2022-10-13 21:36:03 -0700

    c intro

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 /**
  * main - Entry
  * Return:Always 0
- */                                               int main(void)
+ */
+int main(void)
 {
 	printf("Size of a char: %i bytes(s)\n", sizeof(char));
 	printf("Size of an int: %i bytes(s)\n", sizeof(int));
